f973bd2927075c9c67422156d3cf99ccd52677a0
Your Name <crystalgallivan@gmail.com>
2019-08-23 12:04:49 -0600

    Footer, Carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="carousel">
+    <header>
+      <section class="row" id="carousel1">
+        <div id="carouselheader" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="assets/heroimage4.jpg" class="d-block w-100 h100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="assets/heroimage2.jpg" class="d-block w-100 h100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="assets/foodImg.jpg" class="d-block w-100 h100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="assets/capstoneImg.PNG" class="d-block w-100 h100" alt="...">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselheader" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselheader" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </section>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'carousel',
+    components: {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="home">
+    <footer class="container">
+      <section class="row" id="laicons">
+        <div class="col-8" id="frontend">
+          <i class="fab fa-html5 fa-5x"></i>
+          <i class="fab fa-bootstrap fa-5x"></i>
+          <i class="fab fa-js-square fa-5x"></i>
+          <i class="fab fa-vuejs fa-5x"></i>
+          <!-- <i class="fab fa-angular fa-5x"></i> -->
+        </div>
+        <div class="col-4" id="backend">
+          <i class="fab fa-node fa-5x"></i>
+          <i class="fab fa-node-js fa-5x"></i>
+          <!-- <i class="fab fa-python fa-5x"></i> -->
+        </div>
+      </section>
+      <section class="row" id="blank3"></section>
+      <hr id="line" />
+      <section class="row">
+        <div class="col-2" id="blankspace"></div>
+        <div class="col-4" id="socialmedia">
+          <a href="https://www.facebook.com/crystal.gallivan"><i class="fab fa-facebook-square fa-4"></i></a>
+          <!-- <i class="fab fa-instagram fa-4"></i> -->
+          <a href="https://github.com/CrystalGallivan" target="_blank" rel="noopener noreferrer"><i
+              class="fab fa-github fa-4"></i></a>
+          <!-- <i class="fab fa-twitter fa-4"></i> -->
+          <a href="https://www.linkedin.com/in/crystal-gallivan/"><i class="fab fa-linkedin fa-4-in"></i></a>
+          <h1>Contact Me@</h1>
+        </div>
+        <div class="col-6" id="blankspace"></div>
+      </section>
+    </footer>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    components: {
+
+    }
+  }
+</script>
+
+<style>
+  #line {
+    margin-top: 0.1rem;
+    margin-bottom: 0.1rem;
+    border: 0;
+    border-top: 1px solid rgba(0, 0, 0, .1);
+  }
+
+  #socialmedia {
+    background-color: black;
+    color: white;
+    height: 75px;
+    font-size: 20pt;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+  }
+
+  #blankspace {
+    background-color: black;
+    color: white;
+    height: 75px;
+    font-size: 20pt;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    justify-content: space-around;
+
+  }
+
+  #blank3 {
+    background-color: black;
+    height: 15px;
+    outline-color: white;
+  }
+
+  #line {
+    margin-top: 0.1rem;
+    margin-bottom: 0.1rem;
+    border: 0;
+    border-top: 1px solid rgba(0, 0, 0, .1);
+  }
+
+  #socialmedia {
+    background-color: black;
+    color: white;
+    height: 75px;
+    font-size: 20pt;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+  }
+
+  #blankspace {
+    background-color: black;
+    color: white;
+    height: 75px;
+    font-size: 20pt;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    justify-content: space-around;
+
+  }
+</style>
